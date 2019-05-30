c4cde83c36e783ef98af153fc5aac86fa21f8431
Maksym Hnoinskyi <mxmgny@vivaldi.net>
2019-05-30 13:12:39 +0300

    User register route & typo in register form

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from app.models import User
-from wtforms import StringField, PasswordField, SubmitField,
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, ValidationError, EqualTo
 
 
@@ -14,15 +14,15 @@ class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    password1 = PasswordField('Repeat Password', validators=[DataRequired, EqualTo('password')])
+    password1 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
 
-    def validate_username(self,username):
+    def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
             raise ValidationError('This name is already in use')
 
-    def validate_email(self,email):
+    def validate_email(self, email):
         email = User.query.filter_by(email=email.data).first()
         if email is not None:
             raise ValidationError('This email is already in use')
