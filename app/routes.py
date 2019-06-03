5e4d5dfd93bfeb09e4d1587b40bc624bf4aa6a0e
Maksym Hnoinskyi <mxmgny@vivaldi.net>
2019-06-03 18:34:20 +0300

    Pagination for userpage

--- app/routes.py
+++ app/routes.py
@@ -81,8 +81,12 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = posts = user.posts
-    return render_template('user.html', user=user, posts=posts)
+    page = request.args.get('page',1 ,type=int)
+    posts = posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) if posts.has_prev else None
+    return render_template('user.html', user=user, posts=posts.items, prev_url=prev_url, next_url=next_url)
 
 
 
